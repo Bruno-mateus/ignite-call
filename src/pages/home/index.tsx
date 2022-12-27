29ad2dd976277e35889a1e968830de8200a78aef
Bruno-mateus <bruno_205@hotmail.com>
2022-12-27 05:53:16 -0300

    estrutura home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,32 @@
-import { Heading } from '@ignite-ui/react'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+import previewImage from '../../../public/assets/preview.png'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      <Heading as="h1">Hello world</Heading>
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage.src}
+            height={400}
+            width={600}
+            quality={100}
+            priority
+            alt="Calendario simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
     </>
   )
 }
